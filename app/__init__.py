94a117929892228e59bad5492c8a66c2a957e94b
ankman007 <ankeetpoudel6@gmail.com>
2024-11-05 11:51:09 +0545

    init-file-update

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,19 @@ def create_app():
     app = FastAPI()
     
     create_table()
-    download_and_extract_cdx_files()
-    process_cdx_directory('common_crawl_data')
+    # download_and_extract_cdx_files()
+    # process_cdx_directory('common_crawl_data')
     
     from app.views import router as todo_router
     app.include_router(todo_router, prefix='')
     return app 
+
+
+# cdx-00000.gz
+# cdx-00001.gz
+# cdx-00002.gz
+# cdx-00003.gz
+# cdx-00004.gz
+# cdx-00005.gz
+# cdx-00006.gz
+# cdx-00007.gz
